91d4b610038765470fd5c56b708bfca80b497020
Mariano Lischetti <lischettim@gmail.com>
2023-07-25 15:39:46 -0300

    W-13748070: Remove usage of sun.security package (#34)

--- src/main/java/org/mule/weave/v2/jwt/RSAHelper.java
+++ src/main/java/org/mule/weave/v2/jwt/RSAHelper.java
@@ -1,9 +1,15 @@
-import sun.security.util.DerInputStream;
-import sun.security.util.DerValue;
+package org.mule.weave.v2.jwt;
 
 import java.io.IOException;
 import java.math.BigInteger;
-import java.security.*;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.Signature;
+import java.security.SignatureException;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.RSAPrivateCrtKeySpec;
@@ -11,7 +17,7 @@ import java.util.Base64;
 
 public class RSAHelper {
     public static String signString(String content, String privateKeyContent, String algorithm) throws NoSuchAlgorithmException, InvalidKeySpecException, InvalidKeyException, IOException, SignatureException {
-        PrivateKey pk = null;
+        PrivateKey pk;
         if (privateKeyContent.contains("RSA"))
             pk = getPrivatePKCS1Pem(privateKeyContent);
         else
@@ -19,7 +25,7 @@ public class RSAHelper {
 
         Signature privateSignature = Signature.getInstance(algorithm);
         privateSignature.initSign(pk);
-        privateSignature.update(content.getBytes("UTF-8"));
+        privateSignature.update(content.getBytes(StandardCharsets.UTF_8));
         byte[] s = privateSignature.sign();
         return Base64.getUrlEncoder().encodeToString(s).replace("=", "");
     }
@@ -29,20 +35,7 @@ public class RSAHelper {
                 .replace("-----END RSA PRIVATE KEY-----", "");
         byte[] bytes = Base64.getDecoder().decode(privateKeyContent);
 
-        DerInputStream derReader = new DerInputStream(bytes);
-        DerValue[] seq = derReader.getSequence(0);
-        // skip version seq[0];
-        BigInteger modulus = seq[1].getBigInteger();
-        BigInteger publicExp = seq[2].getBigInteger();
-        BigInteger privateExp = seq[3].getBigInteger();
-        BigInteger prime1 = seq[4].getBigInteger();
-        BigInteger prime2 = seq[5].getBigInteger();
-        BigInteger exp1 = seq[6].getBigInteger();
-        BigInteger exp2 = seq[7].getBigInteger();
-        BigInteger crtCoef = seq[8].getBigInteger();
-
-        RSAPrivateCrtKeySpec keySpec =
-                new RSAPrivateCrtKeySpec(modulus, publicExp, privateExp, prime1, prime2, exp1, exp2, crtCoef);
+        RSAPrivateCrtKeySpec keySpec = decodeRSAPrivatePKCS1(bytes);
         KeyFactory keyFactory = KeyFactory.getInstance("RSA");
         PrivateKey privateKey = keyFactory.generatePrivate(keySpec);
         return privateKey;
@@ -57,5 +50,42 @@ public class RSAHelper {
         KeyFactory keyFactory = KeyFactory.getInstance("RSA");
         PrivateKey privateKey = keyFactory.generatePrivate(keySpec);
         return privateKey;
+    }
+
+    private static RSAPrivateCrtKeySpec decodeRSAPrivatePKCS1(byte[] encoded) {
+        ByteBuffer input = ByteBuffer.wrap(encoded);
+        if (der(input, 0x30) != input.remaining()) throw new IllegalArgumentException("Excess data");
+        if (!BigInteger.ZERO.equals(derint(input))) throw new IllegalArgumentException("Unsupported version");
+        BigInteger n = derint(input);
+        BigInteger e = derint(input);
+        BigInteger d = derint(input);
+        BigInteger p = derint(input);
+        BigInteger q = derint(input);
+        BigInteger ep = derint(input);
+        BigInteger eq = derint(input);
+        BigInteger c = derint(input);
+        return new RSAPrivateCrtKeySpec(n, e, d, p, q, ep, eq, c);
+    }
+
+    private static int der(ByteBuffer input, int exp) {
+        int tag = input.get() & 0xFF;
+        if (tag != exp) throw new IllegalArgumentException("Unexpected tag");
+        int n = input.get() & 0xFF;
+        if (n < 128) return n;
+        n &= 0x7F;
+        if ((n < 1) || (n > 2)) throw new IllegalArgumentException("Invalid length");
+        int len = 0;
+        while (n-- > 0) {
+            len <<= 8;
+            len |= input.get() & 0xFF;
+        }
+        return len;
+    }
+
+    private static BigInteger derint(ByteBuffer input) {
+        int len = der(input, 0x02);
+        byte[] value = new byte[len];
+        input.get(value);
+        return new BigInteger(+1, value);
     }
 }
